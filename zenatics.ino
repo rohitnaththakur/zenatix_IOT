ddf016e9738cbf95e6b1d84852222d2e7a02a55f
rohitnaththakur <49039004+rohitnaththakur@users.noreply.github.com>
2020-11-09 17:48:14 +0530

    Add files via upload
    Arudino ide code for sensor data reciving over MQTT protocol

---
+++ zenatics.ino
@@ -0,0 +1,50 @@
+#include <CayenneMQTTESP8266.h>
+#include <DHT.h>
+
+#define CAYENNE_DEBUG
+#define CAYENNE_PRINT Serial
+
+
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Rahul Redmi";
+char pass[] = "7678635151";
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char username[] = "c68e5940-20d6-11eb-883c-638d8ce4c23d";
+char Mqtt_password[] = "d41ea2db2e09c7aab609f66e9ba896ae4846a190";
+char Client_id[] = "9aa55d10-219e-11eb-b767-3f1a8f1211ba";
+ DHT dht(2,DHT11);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Cayenne.begin(username,Mqtt_password,Client_id,ssid, pass);
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+    Cayenne.loop();
+  float temp = dht.readTemperature(true);
+  float hum = dht.readHumidity();
+  Cayenne.virtualWrite(1, temp , TYPE_TEMPERATURE , UNIT_CELSIUS);
+  
+  Cayenne.virtualWrite(2, hum , TYPE_RELATIVE_HUMIDITY , UNIT_PERCENT);
+  //for printing on serial monitor
+    Serial.print("Current humidity = ");
+    Serial.print(hum);
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    Serial.print(temp); 
+    Serial.println("C  ");
+     delay(60000);//Wait 5 seconds before accessing sensor again.
+}
+
+CAYENNE_IN(0)
+{
+  digitalWrite(2,!getValue.asInt());
+}
